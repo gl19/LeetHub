d2459e7f768c8d9ebdcfe944110fb7d457996d84
Xin Liu <55266473+gl19@users.noreply.github.com>
2025-06-02 08:33:46 -0700

    Time: 6 ms (66.19%), Space: 18.8 MB (61.65%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -7,28 +7,25 @@ class Solution:
                 continue
 
             prev = final_state[-1]
+            # Prev going right and current going left, will hit
+            while prev > 0 and a < 0 and abs(a) > abs(prev):
+                final_state.pop()
+                if len(final_state) > 0:
+                    prev = final_state[-1]
+                else:
+                    break
+            
+            if len(final_state) == 0:
+                final_state.append(a)
+                continue
+            
+            # Edge case where asteroids cancel out each other
+            if a < 0 and a == -1 * prev:
+                final_state.pop()
+
             # Same sign or prev and cur asteroids not moving towards each other
             if not (a < 0 and prev > 0):
                 final_state.append(a)
-            else:
-                # Prev going right and current going left, will hit
-                while prev > 0 and a < 0 and abs(a) > abs(prev):
-                    final_state.pop()
-                    if len(final_state) > 0:
-                        prev = final_state[-1]
-                    else:
-                        break
-
-                if len(final_state) == 0:
-                    final_state.append(a)
-                    continue
-
-                # If same abs vals, they negate each other
-                if a == -1 * prev:
-                    final_state.pop()
-
-                if a < 0 and prev < 0:
-                    final_state.append(a)
 
         return final_state
 

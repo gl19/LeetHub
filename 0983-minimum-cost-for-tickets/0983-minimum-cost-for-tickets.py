f80a67fd4fcd9a0e462230cf95f19d5467a7779b
Xin Liu <55266473+gl19@users.noreply.github.com>
2025-06-10 00:21:09 -0700

    Time: 4 ms (56.32%), Space: 17.9 MB (63.29%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
@@ -2,21 +2,16 @@ class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
         max_day = days[-1]
         days_set = set(days)
-        dp = [float("inf")] * (max_day + 1)
-        dp[0] = 0
+        dp = [0] * (max_day + 1)
         for i in range(1, max_day + 1):
             if i in days_set:
-                for j in range(30):
-                    if i + j > max_day:
-                        break
-
-                    if j == 0:
-                        dp[i] = min(dp[i + j], dp[i - 1] + min(costs))
-                    elif j < 7:
-                        dp[i + j] = min(dp[i + j], dp[i - 1] + min(costs[1:]))
-                    elif j < 30:
-                        dp[i + j] = min(dp[i + j], dp[i - 1] + costs[2])
+                dp[i] = min(
+                    dp[max(0, i - 1)] + costs[0],
+                    dp[max(0, i - 7)] + costs[1],
+                    dp[max(0, i - 30)] + costs[2]
+                )
             else:
                 dp[i] = dp[i - 1]
 
+        print(dp)
         return dp[-1]

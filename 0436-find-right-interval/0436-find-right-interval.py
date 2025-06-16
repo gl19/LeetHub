a3811f8fd3b0d2c35aa0d6ed6eeb219edf22414b
Xin Liu <55266473+gl19@users.noreply.github.com>
2025-06-15 22:20:13 -0700

    Time: 52 ms (62.38%), Space: 22.3 MB (19.54%) - LeetHub

---
+++ 0436-find-right-interval/0436-find-right-interval.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findRightInterval(self, intervals: List[List[int]]) -> List[int]:
+        n = len(intervals)
+        start_idx = [(start, i) for i, (start, end) in enumerate(intervals)]
+        start_idx.sort()
+        print(start_idx)
+        def binary_search(end):
+            l, r = 0, n
+            while l < r:
+                mid = (l + r) // 2
+                if start_idx[mid][0] < end:
+                    l = mid + 1
+                else:
+                    r = mid
+
+            return start_idx[l][1] if l < n else -1
+        
+        result = []
+        for interval in intervals:
+            result.append(binary_search(interval[1]))
+        
+        return result
+

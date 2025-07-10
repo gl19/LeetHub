55d4a721fbb6f7806af3775be9e66f5926941332
Xin Liu <55266473+gl19@users.noreply.github.com>
2025-07-09 22:41:18 -0700

    Time: 4 ms (24.23%), Space: 23 MB (5.79%) - LeetHub

---
+++ 0113-path-sum-ii/0113-path-sum-ii.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        paths = []
+        def helper(node, total, path):
+            nonlocal targetSum, paths
+            if not node:
+                return
+
+            total += node.val
+            path.append(node.val)
+
+            if not node.left and not node.right:
+                if total == targetSum:
+                    paths.append(path)
+
+                return
+
+            if node.left:
+                helper(node.left, total, path.copy())
+
+            if node.right:
+                helper(node.right, total, path.copy())
+
+        helper(root, 0, [])
+        return paths
+                
+            

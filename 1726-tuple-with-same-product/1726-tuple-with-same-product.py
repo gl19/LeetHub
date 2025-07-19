5212228a223e6a98dcf340cff0f72915f705f733
Xin Liu <55266473+gl19@users.noreply.github.com>
2025-07-18 17:15:24 -0700

    Time: 1161 ms (12.52%), Space: 104.8 MB (10.14%) - LeetHub

--- 1726-tuple-with-same-product/1726-tuple-with-same-product.py
+++ 1726-tuple-with-same-product/1726-tuple-with-same-product.py
@@ -5,7 +5,6 @@ class Solution:
     def tupleSameProduct(self, nums: List[int]) -> int:
         products = defaultdict(list)
         dedup = list(dict.fromkeys(nums))
-        dedup.sort()
         for i, num1 in enumerate(dedup):
             for num2 in dedup[i + 1:]:
                 products[num1 * num2].append((num1, num2))

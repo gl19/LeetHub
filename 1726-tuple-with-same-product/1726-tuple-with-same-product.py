d6ee64d5a3264eb46a1d0239ee71362481febeb3
Xin Liu <55266473+gl19@users.noreply.github.com>
2025-07-18 17:17:28 -0700

    Time: 407 ms (39.52%), Space: 46.6 MB (27.85%) - LeetHub

--- 1726-tuple-with-same-product/1726-tuple-with-same-product.py
+++ 1726-tuple-with-same-product/1726-tuple-with-same-product.py
@@ -3,15 +3,14 @@ class Solution:
         return sum([i for i in range(1, n)])
 
     def tupleSameProduct(self, nums: List[int]) -> int:
-        products = defaultdict(list)
+        products = defaultdict(int)
         dedup = list(dict.fromkeys(nums))
         for i, num1 in enumerate(dedup):
             for num2 in dedup[i + 1:]:
-                products[num1 * num2].append((num1, num2))
+                products[num1 * num2] += 1
         
         total_pairs = 0
-        for pairs in products.values():
-            n = len(pairs)
+        for n in products.values():
             if n > 1:
                 total_pairs += 8 * self.num_pairs(n)
 

217ff5165e000a7f83ab20f812fe4d7df7bf86e3
Xin Liu <55266473+gl19@users.noreply.github.com>
2026-02-01 01:34:12 -0800

    Time: 912 ms (63.42%), Space: 36.6 MB (79.17%) - LeetHub

--- 3830-longest-alternating-subarray-after-removing-at-most-one-element/3830-longest-alternating-subarray-after-removing-at-most-one-element.py
+++ 3830-longest-alternating-subarray-after-removing-at-most-one-element/3830-longest-alternating-subarray-after-removing-at-most-one-element.py
@@ -1,6 +1,7 @@
 class Solution:
     def longestAlternating(self, nums: List[int]) -> int:
         n = len(nums)
+        
         # down up start: 1 -> 3, even idx are up
         dp_du = [0 for _ in range(n - 1)]
         # up down start: 3 -> 1, odd idx are up
@@ -12,19 +13,13 @@ class Solution:
             is_up = nums[i - 1] < nums[i]
             is_down = nums[i - 1] > nums[i]
 
-            if (is_du_up and is_up) or (not is_du_up and is_down):
-                dp_du[i - 1] = 1
-            else:
-                dp_du[i - 1] = 0
+            dp_du[i - 1] = (is_du_up and is_up) or (not is_du_up and is_down)
+            dp_ud[i - 1] = (is_ud_up and is_up) or (not is_ud_up and is_down)
 
-            if (is_ud_up and is_up) or (not is_ud_up and is_down):
-                dp_ud[i - 1] = 1
-            else:
-                dp_ud[i - 1] = 0
+            # swap up-down and down-up, vice versa
+            is_du_up, is_ud_up = is_ud_up, is_du_up
 
-            is_du_up = not is_du_up
-            is_ud_up = not is_ud_up
-
+        # adds the zigzag seq length backwards for earier compute later
         for i in range(n - 3, -1, -1):
             if dp_du[i] == 1 and dp_du[i + 1] > 0:
                 dp_du[i] += dp_du[i + 1]
@@ -33,34 +28,34 @@ class Solution:
                 dp_ud[i] += dp_ud[i + 1]
 
 
-        print("down-up:", dp_du)
-        print("up-down:", dp_ud)
+        # print("down-up:", dp_du)
+        # print("up-down:", dp_ud)
 
-        # check down-up first and potential for switch
-        max_seq = max(max(dp_du), max(dp_ud)) + 1
+        max_seq = max(max(dp_du), max(dp_ud))
+        # cur_seq just keeps track of the length of the seq before switching
         cur_seq_du = 0
         cur_seq_ud = 0
         for i in range(n - 2):
             # du start switch to ud
             if dp_du[i] == 0:
                 if dp_ud[i + 1] > 0:
-                    max_seq = max(max_seq, cur_seq_du + dp_ud[i + 1] + 1)
+                    max_seq = max(max_seq, cur_seq_du + dp_ud[i + 1])
 
                 cur_seq_du = 0
-            else:
-                if cur_seq_du == 0:
-                    cur_seq_du = dp_du[i]
+            elif cur_seq_du == 0:
+                # reset seq count when seq breaks
+                cur_seq_du = dp_du[i]
 
-            # ud start swithc to du
+            # ud start switch to du
             if dp_ud[i] == 0:
                 if dp_du[i + 1] > 0:
-                    max_seq = max(max_seq, cur_seq_ud + dp_du[i + 1] + 1)
+                    max_seq = max(max_seq, cur_seq_ud + dp_du[i + 1])
 
                 cur_seq_ud = 0
-            else:
-                if cur_seq_ud == 0:
-                    cur_seq_ud = dp_ud[i]
+            elif cur_seq_ud == 0:
+                # reset seq count when seq breaks
+                cur_seq_ud = dp_ud[i]
 
-        return max_seq
+        return max_seq + 1
 
 

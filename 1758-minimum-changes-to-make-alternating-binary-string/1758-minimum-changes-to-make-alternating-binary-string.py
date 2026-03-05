7963bb7c705e0019bd8b6232a1ac9b84e2796b2d
Xin Liu <55266473+gl19@users.noreply.github.com>
2026-03-04 21:53:53 -0800

    Time: 8 ms (55.38%), Space: 19.3 MB (56.18%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.py
@@ -3,17 +3,10 @@ class Solution:
         # starting with 0
         is_zero_start_zero = True
         count_start_zero = 0
-
-        is_zero_start_one = False
-        count_start_one = 0
         for c in s:
             if (c == '0' and not is_zero_start_zero) or (c == '1' and is_zero_start_zero):
                 count_start_zero += 1
 
-            if (c == '0' and not is_zero_start_one) or (c == '1' and is_zero_start_one):
-                count_start_one += 1
-
             is_zero_start_zero = not is_zero_start_zero
-            is_zero_start_one = not is_zero_start_one
 
-        return min(count_start_zero, count_start_one)
+        return min(count_start_zero, len(s) - count_start_zero)
